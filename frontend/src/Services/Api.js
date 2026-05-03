de0de96446c09b5702f3cb1cd806781d8091f6aa
sanjay-arr <sanjay77665522@gmail.com>
2026-05-03 16:53:48 +0530

    Removed fix script and fixed folder issues

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://todo-production-c245.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
